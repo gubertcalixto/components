1016987209fcf57b7610f57c5e5b8d61f1a130b4
Gubert Calixto <eduardogubertcalixto@gmail.com>
2019-10-13 23:48:56 -0300

    Added DragDisabled property

--- src/app/kanban/tokens/card.token.ts
+++ src/app/kanban/tokens/card.token.ts
@@ -2,12 +2,13 @@ export class VsKanbanCard {
     id?: string | number;
     title: string;
     description?: string;
-    canMove?: boolean;
+    canMove = true;
     canDelete?: boolean;
     data?: any;
 
     constructor(card?: VsKanbanCard) {
         this.title = card.title;
         this.id = card.id || this.title;
+        this.canMove = card.canMove;
     }
 }
