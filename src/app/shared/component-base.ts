497461df66d684a35f78a0dbb174b5b4b8ded567
Gubert Calixto <eduardogubertcalixto@gmail.com>
2019-10-25 01:27:30 -0300

    C2 Fixes

--- src/app/shared/component-base.ts
+++ src/app/shared/component-base.ts
@@ -3,7 +3,7 @@ import { Input } from '@angular/core';
 import { PermissionComponentBase } from './permission-base';
 
 export abstract class ComponentBase extends PermissionComponentBase {
-    @Input() tooltip: string;
-    @Input() tooltipPosition: 'left' | 'right' | 'above' | 'below' | 'before' | 'after' = 'below';
-    @Input() classes: string;
+    @Input() public tooltip: string;
+    @Input() public tooltipPosition: 'left' | 'right' | 'above' | 'below' | 'before' | 'after' = 'below';
+    @Input() public classes: string;
 }
